feeeb9cff9b623ad869e94a47a1a3be86c273657
caf-3 <tomas.caetano@ubi.co.mz>
2021-10-09 05:15:08 +0200

    Dockerfile for production added

--- worker/index.js
+++ worker/index.js
@@ -6,7 +6,7 @@ const redisClient = redis.createClient({
   port: keys.redisPort,
   retry_strategy: () => 1000,
 });
-const sub = redisClient.duplicate();
+const sub = redisClient.duplicate(); 
 
 function fib(index) {
   if (index < 2) return 1;
